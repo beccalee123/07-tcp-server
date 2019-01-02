0d86a84648c473aced682c22d4e914292b9a5160
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2019-01-02 07:52:24 -0800

    adds notation

--- modules/command.js
+++ modules/command.js
@@ -6,6 +6,13 @@ const events = require('./events.js');
 const commands = {};
 
 
+/**
+ *
+ *
+ * @param {*} data
+ * @param {*} userId
+ * @param {*} socketPool
+ */
 commands['@all'] =  (data, userId, socketPool) => {
   for( let connection in socketPool ) {
     let user = socketPool[connection];
@@ -13,18 +20,46 @@ commands['@all'] =  (data, userId, socketPool) => {
   }
 };
 
+/**
+ *
+ *
+ * @param {*} data
+ * @param {*} userId
+ * @param {*} socketPool
+ */
 commands['@nick'] =  (data, userId, socketPool) => {
   socketPool[userId].nickname = data.target;
 };
 
+/**
+ *
+ *
+ * @param {*} data
+ * @param {*} userId
+ * @param {*} socketPool
+ */
 commands['@quit'] = (data, userId, socketPool) => {
   socketPool[userId] = null;
 };
 
+/**
+ *
+ *
+ * @param {*} data
+ * @param {*} userId
+ * @param {*} socketPool
+ */
 commands['@list'] = (data, userId, socketPool) => {
   // console.log(chatroom.socketArr);
 };
 
+/**
+ *
+ *
+ * @param {*} data
+ * @param {*} userId
+ * @param {*} socketPool
+ */
 commands['@dm'] = (data, userId, socketPool) => {
   for(let connection in socketPool){
     let user = socketPool[connection];
